fe44e58469e3dc495b8e2e8c3067a07eda635d5e
Christian Kubel Højmose <Christian@hjmose.dk>
2023-03-28 08:16:54 +0200

    Part IV of the tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
     title: 'My first Gatsby Site',
   },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
